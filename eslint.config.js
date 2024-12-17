ee93d8a1457bb8a8f7b826eb150bcb8d1addefc7
AbhishekKolge <abhishekkolge96@gmail.com>
2024-12-17 21:06:03 +0530

    initial setup
    Signed-off-by: AbhishekKolge <abhishekkolge96@gmail.com>

---
+++ eslint.config.js
@@ -0,0 +1,59 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintPluginImport from 'eslint-plugin-import';
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier: eslintPluginPrettier,
+      import: eslintPluginImport,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/ban-ts-comment': 'warn',
+      '@typescript-eslint/no-non-null-assertion': 'warn',
+      'no-console': 'warn',
+      'no-debugger': 'warn',
+      'prefer-const': 'error',
+      'no-var': 'error',
+      'consistent-return': 'warn',
+      'no-duplicate-imports': 'warn',
+      'no-return-await': 'warn',
+      'require-await': 'warn',
+      'prettier/prettier': 'error',
+      'no-undef': 'off',
+      'no-shadow': 'warn',
+      'import/no-duplicates': 'warn',
+      'import/order': ['warn', { 'newlines-between': 'never', alphabetize: { order: 'asc' } }],
+      'import/no-unresolved': 'error',
+      'import/named': 'error',
+      'import/default': 'error',
+    },
+  },
+  {
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json',
+        },
+      },
+    },
+  },
+);
